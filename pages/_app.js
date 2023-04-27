136c01cdb993037218afcd674ab1eb4574a97e72
Timofey25 <kunder211@gmail.com>
2023-04-27 16:19:10 +0300

    fuck ur non

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { WalletProvider } from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WalletProvider>
+      <Component {...pageProps} />
+    </WalletProvider>
+  )
 }
